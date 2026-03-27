df757aebb0ee86c77286746a0c707125a5b09405
tlstjdgus5-creator <tlstjdgus5@gmail.com>
2026-03-27 11:51:06 +0900

    update app icon and PWA2 assets

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,13 +12,13 @@ export default function manifest(): MetadataRoute.Manifest {
     lang: "ko-KR",
     icons: [
       {
-        src: "/pwa-icon-192",
+        src: "/pwa-icon-192.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "maskable",
       },
       {
-        src: "/pwa-icon-512",
+        src: "/pwa-icon-512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
